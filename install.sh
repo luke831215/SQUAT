55d742df471c4e009c8d93fb4f7b8eb931bfdaa5
luke831215 <luke831215@gmail.com>
2018-01-24 16:57:30 +0800

    installation file finished

--- install.sh
+++ install.sh
@@ -3,17 +3,29 @@
 INSTDIR="$( cd "$(dirname "$0")" ; pwd)"
 
 cd ${INSTDIR}
+if [ -d bowtie2 ]; then
+	rm -r bowtie2
+elif [ -d bwa ]; then
+	rm -r bwa
+fi
 
 #install bowtie2
-printf "install bowtie2-2.3.4"
+echo "install bowtie2-2.3.4"
 wget https://downloads.sourceforge.net/project/bowtie-bio/bowtie2/2.3.4/bowtie2-2.3.4-linux-x86_64.zip 
 unzip bowtie2-2.3.4-linux-x86_64.zip
-move bowtie2-2.3.4-linux-x86_64 bowtie2
+rm bowtie2-2.3.4-linux-x86_64.zip
+mv bowtie2-2.3.4-linux-x86_64 bowtie2
+PATH=$PATH:$INSTDIR/bowtie2
 
 #install bwa
-printf "install bwa-0.7.15"
-mkdir -p bwa; cd bwa
+echo "install bwa-0.7.15"
 wget https://downloads.sourceforge.net/project/bio-bwa/bwa-0.7.15.tar.bz2
 tar jxvf bwa-0.7.15.tar.bz2
-mv bwa-0.7.15 bwa;
+rm bwa-0.7.15.tar.bz2
+mv bwa-0.7.15 bwa
 cd bwa; make
+PATH=$PATH:$INSTDIR/bwa
+
+#install python packages
+echo "install python packages"
+pip install fpdf
